51eee902d94e1c2d5d1681b610f07124579e4852
PitiedHarp <165703330+PitiedHarp@users.noreply.github.com>
2024-06-26 21:34:06 -0300

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,10 +1,15 @@
+// index.js
+
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db/db.json');
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+
+const PORT = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
-server.listen(3000, () => {
-  console.log('JSON Server is running');
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
 });
